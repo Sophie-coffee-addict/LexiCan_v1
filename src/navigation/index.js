52368198c9173eb4ab91bcc577f67a90fc957adc
Sophie Cao <sophiecao@Sophies-MacBook-Air-2.local>
2024-03-22 14:13:31 +1300

    initial commit

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -4,8 +4,8 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import SignInScreen from '../screens/SignInScreen';
-import SignUpScreen from '../screens/SignUpScreen';
+//import SignInScreen from '../screens/SignInScreen';
+//import SignUpScreen from '../screens/SignUpScreen';
 // import HomeScreen from '../screens/HomeScreen';
 import SearchScreen from '../screens/SearchScreen';
 import MyWordsScreen from '../screens/MyWordsScreen';
@@ -17,8 +17,8 @@ const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="SignIn" component={SignInScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        {/* <Stack.Screen name="SignIn" component={SignInScreen} /> */}
+        {/* <Stack.Screen name="SignUp" component={SignUpScreen} /> */}
         <Stack.Screen name="SearchScreen" component={SearchScreen} />
         <Stack.Screen name="MyWords" component={MyWordsScreen} />
         {/* <Stack.Screen name="ClickedWord" component={ClickedWordScreen} /> */}
