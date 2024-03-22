b03bebebcb3dbb778de8674929adfda943fb53e5
Sophie Cao <sophiecao@Sophies-MacBook-Air-2.local>
2024-03-22 14:46:44 +1300

    test test

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,6 +3,7 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+//test test
 
 //import SignInScreen from '../screens/SignInScreen';
 //import SignUpScreen from '../screens/SignUpScreen';
